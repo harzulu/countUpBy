6489d802ecf23f4d4c55dd94d1e00a3b4ae053a7
Amanda Otero <mandystar018@gmail.com>
2020-10-27 11:32:19 -0700

    Add function and a branch to return false either if the number is negative or bigger thnt the count to
    Co-authored-by: Joe Nero <josephnero111@gmail.com>,Co-authored-by: Ben McFarland <benrmcfarland@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+function checkNumbers(numberTo, numberBy) {
+  if (numberBy > numberTo || numberTo < 0 || numberBy < 0) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
 $(document).ready(function(){
   $('#form').submit(function(event) {
   event.preventDefault();
@@ -5,9 +14,14 @@ $(document).ready(function(){
   const userNumber1 = parseInt($('#countTo').val());
   const userNumber2 = parseInt($('#countBy').val());
 
-for (let i = userNumber2; i <= userNumber1; i += userNumber2) {
-  alert(i);
-  $('#list').append(`<li>${i}</li>`);
-}
+  if (checkNumbers(userNumber1, userNumber2)) {
+    for (let i = userNumber2; i <= userNumber1; i += userNumber2) {
+      $('#list').append(`<li>${i}</li>`);
+    }
+  }
+  else {
+    alert("Your number(s) are either negative or in the wrong order. Try again");
+  }
+
   });
 });
